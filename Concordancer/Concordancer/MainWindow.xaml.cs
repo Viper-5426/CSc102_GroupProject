197246fb9e1d841ba3367f215b37788025b3cb32
g13m1715 <g13m1715@HUG166>
2018-09-11 16:11:14 +0200

    Input and browsing for text files- select default 'Alice' Removing punctuation complete (efficiently).

--- Concordancer/Concordancer/MainWindow.xaml.cs
+++ Concordancer/Concordancer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace Concordancer
 {
@@ -23,6 +24,49 @@ namespace Concordancer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "TEXT Files | *.txt";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                txtLocation.Text = filename;
+            }
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            string sString = System.IO.File.ReadAllText(@txtLocation.Text);
+
+            txtDisplay.Text = remove_punctuation(sString);  //uses the efficient method to display the book without punctuation
+
+        }
+
+        private string remove_punctuation(string sString)
+        {
+            StringBuilder newString = new StringBuilder("");   //instantiates a stringBuilder object that is mutable, can be changed
+            foreach (char c in sString)  //loops through each character in the book
+            {
+                if (!char.IsPunctuation(c))  //checks whether it is punctuation
+                {
+                    newString.Append(c);  //appends all the letters and spaces to the stringBuilder object 
+                }
+            }
+            return newString.ToString();
         }
     }
 }
